2314c18d9143e2d13d97821288226581dacaba43
devwardhan <devwardhanpathak@gmail.com>
2021-01-25 10:43:45 +0530

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/configureStore';
 import './App.css';
+
+const store = configureStore();
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
